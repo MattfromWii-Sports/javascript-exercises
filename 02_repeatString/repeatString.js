824adb438494436a2f31a471075db166118791df
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-05-13 17:03:54 +0800

    Solve Challenge 2, Repeating Strings Function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let answerAdded='';
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i=0; i < num; i++) {
+            answer=string + answerAdded;
+            answerAdded=answer;
+        }
+        return answerAdded;
+    }
 };
 
 // Do not edit below this line
