d6c6e3fde35d06a6a3fbbb162c8edbd8df2c840a
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-05-13 22:56:59 +0800

    Solve Challenge 3, Reverse String Function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    reverseLetters=words.split('').reverse().join('');
+    return reverseLetters;
 };
 
 // Do not edit below this line
