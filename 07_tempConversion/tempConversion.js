4e64b7a7a7961fa86b3d3d7dec919fee66726126
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-05-16 16:36:20 +0800

    Solve Challenge 7, Temperature Conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  convertCelsius = ((fahr-32) * 5)/9;
+  return Math.round(convertCelsius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  convertFahrenheit = ((cels*9)/5) + 32;
+  return Math.round(convertFahrenheit*10)/10;
 };
 
 // Do not edit below this line
