fa9a23d95af774eaf3d6dcecb677f25a35393f50
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-05-17 13:10:38 +0800

    Solve Challenge 8, Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arrayToSum) {
+  let arraySum = 0;
+	arrayToSum.forEach(item => {
+    arraySum += item;
+  });
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayToMultiple) {
+  let arrayMultiple = 1;
+  arrayToMultiple.forEach(item => {
+    arrayMultiple *= item;
+  });
+  return arrayMultiple;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factor = 1;
+	if (a == 0) {
+    return 1;
+  } else {
+    for (i=a; i>0; i--) {
+      factor = i * factor
+    }
+    return factor;
+  }
 };
 
 // Do not edit below this line
