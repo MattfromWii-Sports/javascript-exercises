7fc57bf9f7a3c78b558602b65d835d912d048226
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-05-17 15:49:17 +0800

    Solve Challenge 10, Fibonacci Sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) return 'OOPS';
+    if (a == 0) return 0;
+    let num1 = 0;
+    let num2 = 1;
+    let sumN = 1;
+    for (i=1; i<a; i++) {
+        sumN = num1 + num2;
+        num1 = num2;
+        num2 = sumN;
+    }
+    return sumN;
 };
 
 // Do not edit below this line
